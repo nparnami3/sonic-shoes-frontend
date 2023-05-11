06217f0c88b9af2864fcef70ef12c22ff2e1bb7f
nparnami3 <87017035+nparnami3@users.noreply.github.com>
2023-05-11 12:47:53 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -84,7 +84,7 @@ const Header = () => {
 
                 <div className="flex items-center gap-2 text-white">
                     {/* Icon start */}
-                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full md:left-[85px] flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative left-[470px]">
+                    <div className="w-8 left-[260px] md:w-12 flex h-8 md:h-12 rounded-full sm:left-[85px]  justify-center items-center hover:bg-black/[0.05] cursor-pointer relative ">
                         <IoMdHeartEmpty className="text-[26px] md:text-[24px]" />
                         <div className="h-[14px] md:h-[18px]  min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[12px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
                             51
@@ -94,7 +94,7 @@ const Header = () => {
 
                     {/* Icon start */}
                     <Link href="/cart">
-                        <div className="w-8 md:w-12 h-8 md:h-12 rounded-full md:left-[75px] left-[480px] flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+                        <div className="w-8 md:w-12 h-8 md:h-12 rounded-full sm:left-[75px] left-[250px] flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
                             <BsCart className="text-[22px] md:text-[20px]" />
                             {cartItems.length > 0 && (
                                 <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
@@ -107,7 +107,7 @@ const Header = () => {
                    
 
                     {/* Mobile icon start */}
-                    <div className="w-24  h-24  rounded-full sm:ml-[450px] flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2 ">
+                    <div className="w-24  h-24  left-[200px] rounded-full sm:ml-[450px] flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2 ">
                         {mobileMenu ? (
                             <VscChromeClose
                                 className="text-[20px] ml-5"
