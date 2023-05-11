084e78bb84950e1f0d6f890e55d7088d6e38bda6
nparnami3 <87017035+nparnami3@users.noreply.github.com>
2023-05-11 13:17:10 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -94,7 +94,7 @@ const Header = () => {
 
                     {/* Icon start */}
                     <Link href="/cart">
-                        <div className="w-8 md:w-12 h-8 md:h-12 rounded-full sm:left-[75px] left-[250px] flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+                        <div className="w-8 md:w-12 h-8 md:h-12 rounded-full sm:left-[75px] left-[250px] flex justify-center items-center hover:bg-black/[0.05] cursor-auto sm:cursor-pointer relative">
                             <BsCart className="text-[22px] md:text-[20px]" />
                             {cartItems.length > 0 && (
                                 <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
